898158ccd23c3e4a968d8d9c5b85d7bb6e5ab575
Brandon Thomas <echelon@gmail.com>
2021-04-05 01:12:55 -0400

    get_env_string_optional: kind of overkill, but completeness
    I have this in yet another project, so I just want a complete coverage
    here.

--- src/lib.rs
+++ src/lib.rs
@@ -103,6 +103,17 @@ pub fn get_env_bool_required(env_name: &str) -> Result<bool, EnvError> {
     })
 }
 
+/// Get an environment variable as an optional `String`.
+pub fn get_env_string_optional(env_name: &str) -> Option<String> {
+  match env::var(env_name).as_ref().ok() {
+    Some(s) => Some(s.to_string()),
+    None => {
+      warn!("Env var '{}' not supplied.", env_name);
+      None
+    },
+  }
+}
+
 /// Get an environment variable as a `String`, or fall back to the provided default.
 pub fn get_env_string_or_default(env_name: &str, default: &str) -> String {
   match env::var(env_name).as_ref().ok() {
